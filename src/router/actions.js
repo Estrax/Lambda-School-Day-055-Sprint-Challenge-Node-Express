1c52aea940221b33bed43765991c6e77b96abc35
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-22 11:31:25 -0400

    added basic CRUD for projects and actions

--- src/router/actions.js
+++ src/router/actions.js
@@ -1,5 +1,68 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../data/helpers/actionModel');
 
+router.route('/')
+    .get(async (req, res) => {
+        try {
+            const actions = await db.get();
+            return res.json(actions);
+        } catch(e) {
+            return res.status(500).json({ error: "Actions could not be retrieved." });
+        }
+    })
+    .post(async (req, res) => {
+        try {
+            const project_id = req.body.project_id;
+            const description = req.body.description;
+            const notes = req.body.notes;
+            const completed = req.body.completed || false;
+            if(!project_id || !description || !notes) return res.status(400).json({ errorMessage: "Please provide all the data for the action." });
+            const newAction = await db.insert({
+                project_id,
+                description,
+                notes,
+                completed
+            })
+            return res.status(201).json(newAction);
+        } catch(e) {
+            return res.status(500).json({ error: "There was an error while saving the action to the database" })
+        }
+    });
+
+router.route('/:id')
+    .get(async (req, res) => {
+        try {
+            const action = await db.get(req.params.id);
+            if(action.length === 0) return res.status(404).json({ message: "The action with the specified ID does not exist." });
+            return res.status(200).json(action);
+        } catch(e) {
+            return res.status(500).json({ error: "The action information could not be retrieved." });
+        }
+    })
+    .put(async (req, res) => {
+        try {
+            const project_id = req.body.project_id;
+            const description = req.body.description;
+            const notes = req.body.notes;
+            const completed = req.body.completed || false;
+
+            if(!project_id || !description || !notes || !completed) return res.status(400).json({ errorMessage: "Please provide all the data for the action." });
+            const actionUpdated = await db.update(req.params.id, { project_id, description, notes, completed })
+            if(action === null) return res.status(404).json({ message: "The action with the specified ID does not exist." });
+            return res.status(200).json(actionUpdated);
+        } catch(e) {
+            return res.status(500).json({ error: "The action information could not be modified." });
+        }
+    })
+    .delete(async (req, res) => {
+        try {
+            const deletedAction = await db.remove(req.params.id);
+            if(deletedAction === 0) return res.status(404).json({ message: "The action with the specified ID does not exist." });
+            return res.status(200).json({...deletedAction, removed: true});
+        } catch(e) {
+            return res.status(500).json({ error: "The action could not be removed" })
+        }
+    });
 
 module.exports = router;
--- src/router/projects.js
+++ src/router/projects.js
@@ -1,5 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../data/helpers/projectModel');
 
+router.route('/')
+    .get(async (req, res) => {
+        try {
+            const projects = await db.get();
+            return res.json(projects);
+        } catch(e) {
+            return res.status(500).json({ error: "Projects could not be retrieved." });
+        }
+    })
+    .post(async (req, res) => {
+        try {
+            const name = req.body.name;
+            const description = req.body.description;
+            const completed = req.body.completed || false;
+            if(!name || !description) return res.status(400).json({ errorMessage: "Please provide all the data for the project." });
+            const newProject = await db.insert({
+                name,
+                description,
+                completed
+            })
+            return res.status(201).json(newProject);
+        } catch(e) {
+            return res.status(500).json({ error: "There was an error while saving the project to the database" })
+        }
+    });
+
+router.route('/:id')
+    .get(async (req, res) => {
+        try {
+            const project = await db.get(req.params.id);
+            if(project.length === 0) return res.status(404).json({ message: "The project with the specified ID does not exist." });
+            return res.status(200).json(project);
+        } catch(e) {
+            return res.status(500).json({ error: "The project information could not be retrieved." });
+        }
+    })
+    .put(async (req, res) => {
+        try {
+            const name = req.body.name;
+            const description = req.body.description;
+            const completed = req.body.completed || false;
+
+            if(!name || !description || !completed) return res.status(400).json({ errorMessage: "Please provide all the data for the project." });
+            const projectUpdated = await db.update(req.params.id, { name, description, completed })
+            if(project === null) return res.status(404).json({ message: "The project with the specified ID does not exist." });
+            return res.status(200).json(projectUpdated);
+        } catch(e) {
+            return res.status(500).json({ error: "The project information could not be modified." });
+        }
+    })
+    .delete(async (req, res) => {
+        try {
+            const deletedProject = await db.remove(req.params.id);
+            if(deletedProject === 0) return res.status(404).json({ message: "The project with the specified ID does not exist." });
+            return res.status(200).json({...deletedProject, removed: true});
+        } catch(e) {
+            return res.status(500).json({ error: "The project could not be removed" })
+        }
+    });
 
 module.exports = router;
