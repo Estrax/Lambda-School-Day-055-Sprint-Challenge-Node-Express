6b73b18d73172f39115f1631b3c3c6697d6a8cea
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-22 11:18:36 -0400

    added default webserver with middleware

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+const express = require('express');
+const initMiddleware = require('./src/serverMiddleware');
+require('dotenv').config();
+
+const app = express();
+initMiddleware(app);
+
+const PORT = process.env.PORT || 5000;
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
+
+app.listen(PORT, (err) => {
+    if(err) throw err;
+    console.log(`App running on ${HOSTNAME} on port ${PORT}`);
+});
