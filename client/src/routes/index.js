d91e1dd689411f6fc9be28be3ad57627b1a0e0cc
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-22 12:10:02 -0400

    added nabar, routes, basic styledcomponents & reactstrap styles

---
+++ client/src/routes/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Router, Switch, Route} from 'react-router-dom';
+import NavbarContainer from '../containers/Navbar';
+// import ActionsPage from '../pages/ActionsPage';
+// import ActionPage from '../pages/ActionPage';
+// import NewActionPage from '../pages/NewActionPage';
+// import EditActionPage from '../pages/EditActionPage';
+// import ProjectsPage from '../pages/ProjectsPage';
+// import ProjectPage from '../pages/ProjectPage';
+// import NewProjectPage from '../pages/NewProjectPage';
+// import EditProjectPage from '../pages/EditProjectPage';
+
+export default (props) => {
+    return (
+        <Router history={props.history}>
+            <div>
+                <NavbarContainer />
+                {/* <Switch>
+                    <Route path="/" exact component={ActionsPage}/>
+                    <Route path="/posts/new" exact component={NewActionPage}/>
+                    <Route path="/posts/:id" exact component={ActionPage}/>
+                    <Route path="/posts/:id/edit" exact component={EditActionPage}/>
+                    <Route path="/users" exact component={ProjectsPage}/>
+                    <Route path="/users/new" exact component={NewProjectPage}/>
+                    <Route path="/users/:id" exact component={ProjectPage}/>
+                    <Route path="/users/:id/edit" exact component={EditProjectPage}/>
+                </Switch> */}
+            </div>
+        </Router>
+    );
+}
---
+++ client/src/styles/index.js
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import {
+    Navbar,
+    NavbarBrand,
+} from 'reactstrap';
+
+export const SubmitBtn = styled.input`
+    // background-color: #000;
+    border: 1px solid black;
+
+    &:hover {
+        // background-color: #fff;
+        // color: #000;
+    }
+`;
+
+export const NavbarBrandStyled = styled(NavbarBrand)`
+    font-size: 24px;
+    color: #fff !important;
+`;
+
+export const NavbarStyled = styled(Navbar)`
+    margin-bottom: 5rem;
+    border-bottom: 1px solid #4c8bf5;
+    background: #4c8bf5;
+`;
+
+export const LinkStyled = styled(Link)`
+    color: white !important;
+`;
+
+export const Card = styled.div`
+    width: 45%;
+    margin: 20px auto;
+
+    &:hover {
+        text-decoration: none;
+    }
+`;
+
+export const CardSingle = styled.div`
+    width: 45%;
+    margin: 20px auto
+`
+
+export const Title = styled.h1`
+    text-align: center;
+    color: black;
+    text-decoration: none;
+    margin-bottom: 0;
+    padding: 10px;
+`;
+
+export const PostTitle = styled.h3`
+    text-align: center;
+    color: black;
+    text-decoration: none;
+    margin-bottom: 0;
+    padding: 10px;
+`;
+
+
+export const Contents = styled.p`
+    color: black;
+    text-decoration: none;
+    text-align: center;
+`;
+
+export const cardBorder = {
+    border: '1px solid black',
+    borderRadius: '6px'
+};
+
+export const ButtonHalf = styled.button`
+    width: 50%;
+    display: inline-block;
+`;
+
+export const Buttons = styled.div`
+    width: 100%;
+`;
+
+export const FormComponent = styled.form`
+    width: 18rem;
+    margin: 0 auto;
+`;
+
+export const StyledLink = styled(Link)`
+    text-decoration: none !important;
+`;
